dd0d31b5ccef8355ef9806cbd372e6054a8bf94a
adail <adail@Adail_Lenovo>
2024-05-31 13:57:52 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/Cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package Cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/* Main é o metodo auto executavel no Java */
+	
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro Programa em Java");
+		
+	}
+
+}
